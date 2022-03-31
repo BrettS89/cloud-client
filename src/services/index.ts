8c0ee04e759b4363ac8ef7fb8e926373d5af4d67
Brett Sodie <blsodie@gmail.com>
2022-03-31 19:11:01 -0400

    fix: rename app to appService

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,9 @@
 import { Application } from '../declarations';
-import app from './app/app.service';
+import appService from './app/app.service';
 import deployment from './deployment/deployment.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
-  app.configure(app);
+  app.configure(appService);
   app.configure(deployment);
 }
